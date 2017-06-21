fc3328ee8da18208b951259092da03af35c55296
Silence in MII <sunbibei@sina.com>
2017-06-21 09:36:41 +0800

    INIT

---
+++ include/middleware/util/log.h
@@ -0,0 +1,32 @@
+/*
+ * utils.h
+ *
+ *  Created on: Jan 16, 2017
+ *      Author: silence
+ */
+
+#ifndef INCLUDE_MIDDLEWARE_UTIL_LOG_H_
+#define INCLUDE_MIDDLEWARE_UTIL_LOG_H_
+
+#include <glog/logging.h>
+#include <glog/log_severity.h>
+
+// #include <iostream>
+
+namespace middleware {
+
+#define LOG_INFO      LOG(INFO)     << "\t"
+#define LOG_WARNING   LOG(WARNING)  << "\t"
+#define LOG_ERROR     LOG(ERROR)    << "\t"
+#define LOG_FATAL     LOG(FATAL)    << "\t"
+
+/*#define LOG_INFO      std::cout    << "\t"
+#define LOG_WARNING   std::cout    << "\t"
+#define LOG_ERROR     std::cout    << "\t"
+#define LOG_FATAL     std::cout    << "\t"*/
+
+} /* namespace middleware */
+
+
+
+#endif /* INCLUDE_MIDDLEWARE_UTIL_LOG_H_ */
